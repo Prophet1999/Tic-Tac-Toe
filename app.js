7a51245c1d4eaa4a3b4ce1a00ec7b1536eef0814
Himanshu Patel <88833043+Prophet1999@users.noreply.github.com>
2024-12-04 01:21:16 +0530

    optimizing and correcting JS syntax

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const winPatterns = [
   [2, 5, 8],
   [2, 4, 6],
   [3, 4, 5],
-  [6, 7, 8],
+  [6, 7, 8]
 ];
 
 const resetGame = () => {
@@ -41,7 +41,7 @@ boxes.forEach((box) => {
 
     let isWinner = checkWinner();
 
-    if (count === 9 && !isWinner) {
+    if (count == 9 && !isWinner) {
       gameDraw();
     }
   });
@@ -78,13 +78,12 @@ const checkWinner = () => {
     let pos2Val = boxes[pattern[1]].innerText;
     let pos3Val = boxes[pattern[2]].innerText;
 
-    if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
-      if (pos1Val === pos2Val && pos2Val === pos3Val) {
+    if (pos1Val != "" && pos2Val != "" && pos3Val != "" && pos1Val === pos2Val && pos2Val === pos3Val) {
         showWinner(pos1Val);
         return true;
       }
-    }
   }
+  return false;
 };
 
 newGameBtn.addEventListener("click", resetGame);
